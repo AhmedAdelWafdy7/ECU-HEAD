08325cb7c896cf4fb196519b31f65a35ccf1dee7
AhmedAdelWafdy7 <fandes99@gmail.com>
2024-09-06 12:20:36 +0300

    feat: Add initial implementation of CircularGauge component in QML

---
+++ ECU-HEAD.pro
@@ -0,0 +1,79 @@
+QT += quick qml core
+QT += 3dcore 3drender 3dquick 3dquick-private
+
+# You can make your code fail to compile if it uses deprecated APIs.
+# In order to do so, uncomment the following line.
+#DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
+
+SOURCES += \
+        main.cpp \
+        scenehelper.cpp
+
+RESOURCES += qml.qrc
+
+# Additional import path used to resolve QML modules in Qt Creator's code model
+QML_IMPORT_PATH =
+
+# Additional import path used to resolve QML modules just for Qt Quick Designer
+QML_DESIGNER_IMPORT_PATH =
+
+# Default rules for deployment.
+qnx: target.path = /tmp/$${TARGET}/bin
+else: unix:!android: target.path = /opt/$${TARGET}/bin
+!isEmpty(target.path): INSTALLS += target
+
+DISTFILES += \
+    icons/Road/Frame 33.png \
+    icons/Road/SpeedLimit.svg \
+    icons/Road/car.png \
+    icons/Road/carRoad.svg \
+    icons/Road/mdi_clock-time-four-outline.svg \
+    icons/Road/mdi_map-marker-outline.svg \
+    icons/Road/mdi_turn-right-bold.svg \
+    icons/Road/mingcute_steering-wheel-fill.svg \
+    icons/Road/road.png \
+    icons/Road/road2.png \
+    icons/Road/road3.svg \
+    icons/Road/ss.svg \
+    icons/Vector 1.png \
+    icons/Vector 1.svg \
+    icons/Vector 2.png \
+    icons/Vector 2.svg \
+    icons/bottom.png \
+    icons/cloud.svg \
+    icons/desal.svg \
+    icons/feaul.svg \
+    icons/icons-left-checked/icon-park-solid_right-two.svg \
+    icons/icons-left-checked/mdi_car-battery.svg \
+    icons/icons-left-checked/mdi_car-handbrake.svg \
+    icons/icons-left-checked/mdi_car-tire-alert.svg \
+    icons/icons-left-checked/mdi_oil.svg \
+    icons/icons-left-checked/ph_engine-bold.svg \
+    icons/icons-left/icon-park-solid_right-two.svg \
+    icons/icons-left/mdi_car-battery.svg \
+    icons/icons-left/mdi_car-handbrake.svg \
+    icons/icons-left/mdi_car-tire-alert.svg \
+    icons/icons-left/mdi_oil.svg \
+    icons/icons-left/ph_engine-bold.svg \
+    icons/icons-right-checked/icon-park-solid_right-two.svg \
+    icons/icons-right-checked/mdi_car-brake-parking.svg \
+    icons/icons-right-checked/mdi_car-light-dimmed.svg \
+    icons/icons-right-checked/mdi_car-light-fog.svg \
+    icons/icons-right-checked/mdi_car-light-high.svg \
+    icons/icons-right-checked/mdi_seatbelt.svg \
+    icons/icons-right/icon-park-solid_right-two.svg \
+    icons/icons-right/mdi_car-brake-parking.svg \
+    icons/icons-right/mdi_car-light-dimmed.svg \
+    icons/icons-right/mdi_car-light-fog.svg \
+    icons/icons-right/mdi_car-light-high.svg \
+    icons/icons-right/mdi_seatbelt.svg \
+    icons/left.svg
+
+
+# Default rules for deployment.
+qnx: target.path = /tmp/$${TARGET}/bin
+else: unix:!android: target.path = /home/$${TARGET}/bin
+!isEmpty(target.path): INSTALLS += target
+
+HEADERS += \
+    scenehelper.h
---
+++ build/Desktop_Qt_5_15_17_GCC_64bit-Profile/qmlcache_loader.cpp
@@ -0,0 +1,120 @@
+#include <QtQml/qqmlprivate.h>
+#include <QtCore/qdir.h>
+#include <QtCore/qurl.h>
+
+namespace QmlCacheGeneratedCode {
+namespace _0x5f__CircularGauge_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _0x5f__CarLoader_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _0x5f__CarModel_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _0x5f__main_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _0x5f__DashboardModel_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _0x5f__main2_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _0x5f__DashboardView_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _0x5f__CenterStack_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _0x5f__IconButton_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _0x5f__CarView_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+
+}
+namespace {
+struct Registry {
+    Registry();
+    ~Registry();
+    QHash<QString, const QQmlPrivate::CachedQmlUnit*> resourcePathToCachedUnit;
+    static const QQmlPrivate::CachedQmlUnit *lookupCachedUnit(const QUrl &url);
+};
+
+Q_GLOBAL_STATIC(Registry, unitRegistry)
+
+
+Registry::Registry() {
+        resourcePathToCachedUnit.insert(QStringLiteral("/CircularGauge.qml"), &QmlCacheGeneratedCode::_0x5f__CircularGauge_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/CarLoader.qml"), &QmlCacheGeneratedCode::_0x5f__CarLoader_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/CarModel.qml"), &QmlCacheGeneratedCode::_0x5f__CarModel_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/main.qml"), &QmlCacheGeneratedCode::_0x5f__main_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/DashboardModel.qml"), &QmlCacheGeneratedCode::_0x5f__DashboardModel_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/main2.qml"), &QmlCacheGeneratedCode::_0x5f__main2_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/DashboardView.qml"), &QmlCacheGeneratedCode::_0x5f__DashboardView_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/CenterStack.qml"), &QmlCacheGeneratedCode::_0x5f__CenterStack_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/IconButton.qml"), &QmlCacheGeneratedCode::_0x5f__IconButton_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/CarView.qml"), &QmlCacheGeneratedCode::_0x5f__CarView_qml::unit);
+    QQmlPrivate::RegisterQmlUnitCacheHook registration;
+    registration.version = 0;
+    registration.lookupCachedQmlUnit = &lookupCachedUnit;
+    QQmlPrivate::qmlregister(QQmlPrivate::QmlUnitCacheHookRegistration, &registration);
+}
+
+Registry::~Registry() {
+    QQmlPrivate::qmlunregister(QQmlPrivate::QmlUnitCacheHookRegistration, quintptr(&lookupCachedUnit));
+}
+
+const QQmlPrivate::CachedQmlUnit *Registry::lookupCachedUnit(const QUrl &url) {
+    if (url.scheme() != QLatin1String("qrc"))
+        return nullptr;
+    QString resourcePath = QDir::cleanPath(url.path());
+    if (resourcePath.isEmpty())
+        return nullptr;
+    if (!resourcePath.startsWith(QLatin1Char('/')))
+        resourcePath.prepend(QLatin1Char('/'));
+    return unitRegistry()->resourcePathToCachedUnit.value(resourcePath, nullptr);
+}
+}
+int QT_MANGLE_NAMESPACE(qInitResources_qml)() {
+    ::unitRegistry();
+    Q_INIT_RESOURCE(qml_qmlcache);
+    return 1;
+}
+Q_CONSTRUCTOR_FUNCTION(QT_MANGLE_NAMESPACE(qInitResources_qml))
+int QT_MANGLE_NAMESPACE(qCleanupResources_qml)() {
+    Q_CLEANUP_RESOURCE(qml_qmlcache);
+    return 1;
+}

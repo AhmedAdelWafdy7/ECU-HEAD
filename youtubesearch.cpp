5ae2d12cf97f11c7f549053682f735bd0dfd843c
AhmedAdelWafdy7 <fandes99@gmail.com>
2025-04-16 16:05:44 +0200

    fix: make youtube api environment variable

--- youtubesearch.cpp
+++ youtubesearch.cpp
@@ -1,4 +1,5 @@
 #include "youtubesearch.h"
+#include <cstdlib> 
 
 YouTubeSearch::YouTubeSearch() {
     manager = new QNetworkAccessManager(this);
@@ -7,14 +8,24 @@ YouTubeSearch::YouTubeSearch() {
 
 void YouTubeSearch::searchVideos(const QString& query) {
     emit searchStarted();
+    
+    // Retrieve the API key from the environment variable
+    const char* apiKey = std::getenv("YOUTUBE_API_KEY");
+    if (!apiKey) {
+        qDebug() << "Error: YOUTUBE_API_KEY environment variable not set!";
+        emit searchError("API key not set");
+        return;
+    }
+
+
     QNetworkRequest request(QUrl("https://www.googleapis.com/youtube/v3/search?part=snippet&q=" + query + 
-    "&maxResults=5&key=YOUTUBE_API_KEY"));
+                                 "&maxResults=5&key=" + QString(apiKey)));
 
     QSslConfiguration conf = request.sslConfiguration();
     conf.setPeerVerifyMode(QSslSocket::VerifyNone);
     QFile certFile("/etc/ssl/certs/youtube.pem");
     
-    if(certFile.open(QIODevice::ReadOnly)) {
+    if (certFile.open(QIODevice::ReadOnly)) {
         QSslCertificate cert(&certFile, QSsl::Pem);
         conf.setCaCertificates(QList<QSslCertificate>() << cert);
     }
@@ -26,12 +37,11 @@ void YouTubeSearch::searchVideos(const QString& query) {
 
 void YouTubeSearch::handleNetworkData(QNetworkReply *reply) {
     auto deferSearchFinished = qScopeGuard([this](){ emit searchFinished(); });
-    if(reply->error() == QNetworkReply::NoError) {
+    if (reply->error() == QNetworkReply::NoError) {
         QByteArray responseData = reply->readAll();
         QJsonDocument jsonDoc = QJsonDocument::fromJson(responseData);
         QJsonObject jsonObj = jsonDoc.object();
         QJsonArray jsonArray = jsonObj["items"].toArray();
-
 
         // Send data to QML
         emit searchResultsReady(jsonArray);
